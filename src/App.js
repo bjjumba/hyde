94116cd3f89f1b2dc032915f3cbfcf91f09ed750
bjjumba <ericjjumba5@gmail.com>
2022-10-21 19:53:47 +0300

    new1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import NavBar from './components/NavBar'
-import Home from './components/Home'
 import About from './components/About'
-import Services from './components/Services'
 import Products from './components/Projects'
 import Team from './components/Team'
 import Footer from './components/Footer'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import './App.css';
 
 function App() {
+
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+      // Update network status
+      const handleStatusChange = () => {
+        setIsOnline(navigator.onLine);
+      };
+  
+      // Listen to the online status
+      window.addEventListener('online', handleStatusChange);
+  
+      // Listen to the offline status
+      window.addEventListener('offline', handleStatusChange);
+       //send a toast message
+       isOnline?toast.success("You are Online"):toast.error("Check your Network Connection")
+      // Specify how to clean up after this effect for performance improvment
+      return () => {
+        window.removeEventListener('online', handleStatusChange);
+        window.removeEventListener('offline', handleStatusChange);
+      };
+  }, [isOnline]);
+  
   return (
-    <>
+    <> 
+    <ToastContainer/>
         <NavBar/>
         <About/>
         {/* <Home/> */}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,9 @@ const NavBar = () => {
     }
   return (
     <header className="header">
-      <Link to="/" className="Logo">Hyde Innovations</Link>
+      <Link to="/" className="Logo">
+        <img src="https://res.cloudinary.com/di4su7hro/image/upload/v1666369854/HYDE%20UG/hyde_gkggz1.jpg" class="mr-3 h-8" alt="FlowBite Logo"/>
+      </Link>
 
       <nav className={nav?'navbar active':'navbar'}>
          <Link to= "home" smooth={true} duration={500} >Home</Link>
